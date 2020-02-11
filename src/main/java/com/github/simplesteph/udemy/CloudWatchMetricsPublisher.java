a41cb04f6dd8647068a40470cec5d8588192ac67
Kritarth <kritarth.dodia@rci.rogers.com>
2020-02-11 11:00:23 -0500

    Adding multiple dimensions to the custom cloud watch metrics

--- src/main/java/com/github/simplesteph/udemy/CloudWatchMetricsPublisher.java
+++ src/main/java/com/github/simplesteph/udemy/CloudWatchMetricsPublisher.java
@@ -7,23 +7,36 @@ import com.amazonaws.services.cloudwatch.model.MetricDatum;
 import com.amazonaws.services.cloudwatch.model.PutMetricDataRequest;
 import com.amazonaws.services.cloudwatch.model.PutMetricDataResult;
 import com.amazonaws.services.cloudwatch.model.StandardUnit;
+import com.amazonaws.util.EC2MetadataUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CloudWatchMetricsPublisher{
 
     public void publishResponseTime(String apiName, long milliSeconds, AmazonCloudWatch cw){
 
-        Dimension dimension = new Dimension()
+        Dimension instanceIdDimension = new Dimension()
+                .withName("InstanceId")
+                .withValue(EC2MetadataUtils.getInstanceId());
+
+        Dimension apiNameDimension = new Dimension()
                 .withName("API")
                 .withValue(apiName);
+
+        List<Dimension> dimensions = new ArrayList<>();
+        dimensions.add(instanceIdDimension);
+        dimensions.add(apiNameDimension);
 
         MetricDatum datum = new MetricDatum()
                 .withMetricName("API_Response_Time")
                 .withUnit(StandardUnit.Milliseconds)
                 .withValue(Double.valueOf(milliSeconds))
-                .withDimensions(dimension);
+                .withDimensions(dimensions);
 
         PutMetricDataRequest request = new PutMetricDataRequest()
-                .withNamespace("EC2/APIs")
+                .withNamespace("EC2")
                 .withMetricData(datum);
 
         PutMetricDataResult response = cw.putMetricData(request);
